dd6c3278714138a386737b56eaf175503d8abe64
ferris <lucasf@digitalev.com>
2010-12-21 19:07:45 -0600

    attempt to further fix references.  attempt to instantiate runtime in SimpleCqrsRegistration

---
+++ src/Paragon.ContentTree.Domain/WebRootSimpleCqrsRuntime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleCqrs;
+
+namespace Paragon.ContentTree.Domain
+{
+	public class WebRootSimpleCqrsRuntime : SimpleCqrsRuntime<SimpleCqrs.Unity.UnityServiceLocator>
+	{
+	}
+}
