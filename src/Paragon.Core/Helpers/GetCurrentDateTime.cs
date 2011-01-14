99360474b238c7d2ca67957ce2a16b4d640d4f51
Ferris Lucas <lucasf@digitalev.com>
2011-01-13 23:27:37 -0600

    add GetCurrentDateTime helper to core

---
+++ src/Paragon.Core/Helpers/GetCurrentDateTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Paragon.Core.Helpers
+{
+	public interface IGetCurrentDateTime
+	{
+		DateTime Now();
+	}
+
+	public class GetCurrentDateTime : IGetCurrentDateTime
+	{
+		public DateTime Now()
+		{
+			return DateTime.Now;
+		}
+	}
+}
