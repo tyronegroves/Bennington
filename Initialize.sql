b45556dde23a3b7e2c7f154bb05e7f4665e63700
Ferris Lucas <lucasf@digitalev.com>
2011-01-19 01:47:02 -0600

    update Initialize.sql to reflect new db schema

--- Initialize.sql
+++ Initialize.sql
@@ -9,24 +9,6 @@ CREATE TABLE TreeNode
 	ParentTreeNodeId nvarchar(100)
 );
 
-CREATE TABLE [dbo].[ContentTreeNode](
-	[Key] [int] IDENTITY(1,1) NOT NULL,
-	[CreateDate] [datetime] NULL,
-	[CreateBy] [nvarchar](500) NULL,
-	[LastModifyDate] [datetime] NULL,
-	[LastModifyBy] [nvarchar](500) NULL,
-	[TreeNodeId] [nvarchar](100) NULL,
-	[UrlSegment] [nvarchar](500) NULL,
-	[Sequence] [int] NULL,
-	[Name] [nvarchar](100) NULL,
-	[Content] [ntext] NULL,
-	[ContentItemId] [nvarchar] (100) NULL,
- CONSTRAINT [PK_ContentTreeNode] PRIMARY KEY CLUSTERED 
-(
-	[Key] ASC
-)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
 GO
 
 CREATE TABLE [dbo].[ContentTreeSectionNode](
@@ -49,9 +31,29 @@ CREATE TABLE [dbo].[ContentTreeSectionNode](
 
 GO
 
+CREATE TABLE [dbo].[ContentNodeProviderDraft](
+	[Key] [int] IDENTITY(1,1) NOT NULL,
+	[CreateDate] [datetime] NULL,
+	[CreateBy] [nvarchar](500) NULL,
+	[LastModifyDate] [datetime] NULL,
+	[LastModifyBy] [nvarchar](500) NULL,
+	[TreeNodeId] [nvarchar](100) NULL,
+	[UrlSegment] [nvarchar](500) NULL,
+	[Sequence] [int] NULL,
+	[Name] [nvarchar](100) NULL,
+	[Action] [nvarchar](100) NULL,
+	[MetaTitle] [nvarchar](500) NULL,
+	[MetaDescription] [ntext] NULL,
+	[HeaderText] [nvarchar](500) NULL,
+	[Body] [ntext] NULL,
+	[MetaKeyword] [ntext] NULL,
+ CONSTRAINT [PK_ContentNodeProviderDraft] PRIMARY KEY CLUSTERED 
+(
+	[Key] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-Insert Into dbo.[ContentTreeNode] (TreeNodeId, UrlSegment, Sequence, [ContentItemId]) Values ('00000000-0000-0000-0000-000000000000', '', Null, 'Index');
-Insert Into dbo.[ContentTreeNode] (TreeNodeId, UrlSegment, Sequence, [Name], [ContentItemId]) Values ('3333e2e3-e968-44b3-b2ba-1f86a2481ce2', 'TestPage', 10, 'Test Page', 'Index');
+GO
 
-Insert Into dbo.[TreeNode] (Id, [Type], ParentTreeNodeId) Values ('00000000-0000-0000-0000-000000000000', 'Paragon.ContentTree.ContentNodeProvider.ContentNodeProvider', '-1');
-Insert Into dbo.[TreeNode] (Id, [Type], ParentTreeNodeId) Values ('3333e2e3-e968-44b3-b2ba-1f86a2481ce2', 'Paragon.ContentTree.ContentNodeProvider.ContentNodeProvider', '00000000-0000-0000-0000-000000000000');
+Insert Into dbo.[TreeNode] (Id, [Type], ParentTreeNodeId) Values ('00000000-0000-0000-0000-000000000000', 'Paragon.ContentTree.ContentNodeProvider.ContentNodeProvider, Paragon.ContentTree.ContentNodeProvider, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null', '');
+INSERT INTO [dbo].[ContentNodeProviderDraft] ([TreeNodeId]) VALUES ('00000000-0000-0000-0000-000000000000')
