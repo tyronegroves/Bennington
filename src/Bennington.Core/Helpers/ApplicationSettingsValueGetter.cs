887fd2a51bda9c267ac584acddcc791653097c17
Darren Cauthon <darren@cauthon.com>
2011-05-22 23:19:33 -0500

    Comment out a helper class.

--- src/Bennington.Core/Helpers/ApplicationSettingsValueGetter.cs
+++ src/Bennington.Core/Helpers/ApplicationSettingsValueGetter.cs
@@ -2,16 +2,16 @@
 
 namespace Bennington.Core.Helpers
 {
-	public interface IApplicationSettingsValueGetter
-	{
-		string GetValue(string key);
-	}
+    public interface IApplicationSettingsValueGetter
+    {
+        string GetValue(string key);
+    }
 
-	public class ApplicationSettingsValueGetter : IApplicationSettingsValueGetter
-	{
-		public string GetValue(string key)
-		{
-			return ConfigurationManager.AppSettings[key];
-		}
-	}
+    public class ApplicationSettingsValueGetter : IApplicationSettingsValueGetter
+    {
+        public string GetValue(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
 }
--- src/Bennington.Core/Helpers/GetParentRouteDataDictionaryFromChildActionRouteData.cs
+++ src/Bennington.Core/Helpers/GetParentRouteDataDictionaryFromChildActionRouteData.cs
@@ -1,36 +1,36 @@
-﻿using System.Web.Mvc;
-using System.Web.Routing;
+﻿//using System.Web.Mvc;
+//using System.Web.Routing;
 
-namespace Bennington.Core.Helpers
-{
-	///<summary>
-	///</summary>
-	public interface IGetParentRouteDataDictionaryFromChildActionRouteData
-	{
-		///<summary>
-		///</summary>
-		///<param name="childActionRouteData"></param>
-		///<returns></returns>
-		RouteData GetRouteValues(RouteData childActionRouteData);
-	}
+//namespace Bennington.Core.Helpers
+//{
+//    ///<summary>
+//    ///</summary>
+//    public interface IGetParentRouteDataDictionaryFromChildActionRouteData
+//    {
+//        ///<summary>
+//        ///</summary>
+//        ///<param name="childActionRouteData"></param>
+//        ///<returns></returns>
+//        RouteData GetRouteValues(RouteData childActionRouteData);
+//    }
 
-	///<summary>
-	///</summary>
-	public class GetParentRouteDataDictionaryFromChildActionRouteData : IGetParentRouteDataDictionaryFromChildActionRouteData
-	{
-		public RouteData GetRouteValues(RouteData childActionRouteData)
-		{
-			if (childActionRouteData == null) return null;
-			if (childActionRouteData.DataTokens != null)
-			{
-				var parentActionViewContext = ((ViewContext)childActionRouteData.DataTokens["ParentActionViewContext"]);
-				if (parentActionViewContext != null)
-				{
-					return parentActionViewContext.RouteData;
-				}
-			}
-			return null;
-		}
-	}
+//    ///<summary>
+//    ///</summary>
+//    public class GetParentRouteDataDictionaryFromChildActionRouteData : IGetParentRouteDataDictionaryFromChildActionRouteData
+//    {
+//        public RouteData GetRouteValues(RouteData childActionRouteData)
+//        {
+//            if (childActionRouteData == null) return null;
+//            if (childActionRouteData.DataTokens != null)
+//            {
+//                var parentActionViewContext = ((ViewContext)childActionRouteData.DataTokens["ParentActionViewContext"]);
+//                if (parentActionViewContext != null)
+//                {
+//                    return parentActionViewContext.RouteData;
+//                }
+//            }
+//            return null;
+//        }
+//    }
 
-}
+//}
