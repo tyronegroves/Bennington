3df1c4f7416f7baa8915dc73a5ae3550a155106d
Darren Cauthon <darren@cauthon.com>
2011-05-24 22:25:14 -0500

    Get header styles to fill the appropriate space, but with a fake item.

---
+++ src/SampleApp/Controllers/LocationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MvcTurbine.Routing;
+
+namespace SampleApp.Controllers
+{
+    public class LocationRouting : IRouteRegistrator
+    {
+        public void Register(RouteCollection routes)
+        {
+            routes.MapRoute(
+                    null,
+                    "Manage/Location/{action}",
+                    new { controller = "Location", action = "Index" }
+                    );
+        }
+    }
+
+    public class LocationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/SampleApp/Views/Location/Index.aspx
@@ -0,0 +1,8 @@
+<%@ Page Title="" Language="C#" Inherits="System.Web.Mvc.ViewPage" MasterPageFile="~/Views/Shared/ManageSite.Master" %>
+
+<asp:Content runat="server" ID="Title" ContentPlaceHolderID="TitleContent"></asp:Content>
+<asp:Content runat="server" ID="Main" ContentPlaceHolderID="MainContent">
+<div id="content_container"><div id="pageheader" class="clearfix"><h1>Welcome to the Admin System</h1></div><div class="section">   <ul class="tabs"><li>Dashboard Display</li></ul></div><div id="tab1" class="tabContent"><div class="section"><div class="highlight"><div class="content"></div></div></div>				   </div>
+			</div>
+
+</asp:Content>
