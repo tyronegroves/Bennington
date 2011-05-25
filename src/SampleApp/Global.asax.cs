a693d794e0ecccedefe9aa664b4a3deb38a30754
Darren Cauthon <darren@cauthon.com>
2011-05-24 21:45:08 -0500

    Get the basic SMS to work.

---
+++ src/SampleApp/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using MvcTurbine.ComponentModel;
+using MvcTurbine.Unity;
+using MvcTurbine.Web;
+
+namespace SampleApp
+{
+    public class MvcApplication : TurbineApplication
+    {
+        static MvcApplication()
+        {
+            ServiceLocatorManager.SetLocatorProvider(() => new UnityServiceLocator());
+        }
+    }
+}
