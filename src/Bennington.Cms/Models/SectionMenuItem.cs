de82bd422152c3416396cf1fae2a89410bf1ef73
Darren Cauthon <darren@cauthon.com>
2011-06-04 15:58:38 -0500

    Add files from previous commit.

---
+++ src/Bennington.Cms/Models/SectionMenuItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Bennington.Cms.Models
+{
+    public class SectionMenuItem
+    {
+        public string Name { get; set; }
+    }
+
+    public class SectionMenuItemForAControllerAction : SectionMenuItem
+    {
+        public string Controller { get; set; }
+
+        public string Action { get; set; }
+    }
+}
