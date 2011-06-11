929f54c480be28ef9d8f14b0a97933238af17efd
Darren Cauthon <darren@cauthon.com>
2011-06-11 14:08:52 -0500

    Flush out css and javascript from sample app.

--- src/SampleApp/Scripts/Site.js
+++
@@ -1,9 +0,0 @@
-﻿
-(function ($) {
-
-    $(window).load(function () {
-        $('#data_table').find('tr:odd').addClass('odd');
-        $('#data_table').find('tr:even').addClass('even');
-    });
-
-} (jQuery));
