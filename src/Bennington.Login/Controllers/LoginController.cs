9424fb3406f9c35b0fe580948ff7c38a7e7ad672
Darren Cauthon <darren@cauthon.com>
2011-08-13 16:34:56 -0500

    Add filter to force a user to go to login if not logged in.

--- src/Bennington.Login/Controllers/LoginController.cs
+++ src/Bennington.Login/Controllers/LoginController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Web.Mvc;
 using Bennington.Cms.Attributes;
+using Bennington.Login.Attributes;
 using Bennington.Login.Models;
 
 namespace Bennington.Login.Controllers
 {
     [DoNotUseTheDefaultMasterPage]
+    [DoNotRequireAuthentication]
     public class LoginController : Controller
     {
         public ActionResult Index()
