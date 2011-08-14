64b764fe41e7d3f61327d6ab94ff38e1f1529703
Darren Cauthon <darren@cauthon.com>
2011-08-13 19:26:18 -0500

    Implement the login controller into the sample app, tests.

---
+++ src/Bennington.Login.Tests/with_automoqer.cs
@@ -0,0 +1,30 @@
+﻿using AutoMoq;
+using Moq;
+
+namespace Bennington.Login.Tests
+{
+    public class with_automoqer
+    {
+        public static AutoMoqer mocker;
+
+        public with_automoqer()
+        {
+            mocker = new AutoMoqer();
+        }
+
+        public static T Create<T>()
+        {
+            return mocker.Create<T>();
+        }
+
+        public static Mock<T> GetMock<T>() where T : class
+        {
+            return mocker.GetMock<T>();
+        }
+
+        public static void SetInstance<T>(T instance) where T : class
+        {
+            mocker.SetInstance(instance);
+        }
+    }
+}
--- src/Bennington.Login/Controllers/LoginController.cs
+++ src/Bennington.Login/Controllers/LoginController.cs
@@ -1,7 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using Bennington.Cms.Attributes;
 using Bennington.Login.Attributes;
 using Bennington.Login.Models;
+using MvcTurbine.MembershipProvider;
 
 namespace Bennington.Login.Controllers
 {
@@ -9,6 +11,14 @@ namespace Bennington.Login.Controllers
     [DoNotRequireAuthentication]
     public class LoginController : Controller
     {
+        private const string InvalidLoginFormMessage = "Login Failed. Please try again.";
+        private readonly IMembershipService membershipService;
+
+        public LoginController(IMembershipService membershipService)
+        {
+            this.membershipService = membershipService;
+        }
+
         public ActionResult Index()
         {
             return View("Index", new LoginForm());
@@ -17,7 +27,27 @@ namespace Bennington.Login.Controllers
         [HttpPost]
         public ActionResult Index(LoginForm loginForm)
         {
-            return View("Index", new LoginForm());
+            return ThisIsAValidLogin(loginForm)
+                       ? LoginAndRedirect(loginForm)
+                       : ReturnTheFormAsAnError(loginForm);
+        }
+
+        private ActionResult ReturnTheFormAsAnError(LoginForm loginForm)
+        {
+            foreach (var field in typeof (LoginForm).GetProperties().Select(x => x.Name))
+                ModelState.AddModelError(field, InvalidLoginFormMessage);
+            return View("Index", loginForm);
+        }
+
+        private ActionResult LoginAndRedirect(LoginForm loginForm)
+        {
+            membershipService.LogInAsUser(loginForm.Username, loginForm.Password);
+            return Redirect("/MANAGE");
+        }
+
+        private bool ThisIsAValidLogin(LoginForm loginForm)
+        {
+            return membershipService.ValidateUser(loginForm.Username, loginForm.Password);
         }
     }
 }
