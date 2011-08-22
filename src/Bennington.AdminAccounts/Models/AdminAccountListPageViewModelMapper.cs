eaea19caf4ee7ce188b19c57be68e5b902e2f156
Darren Cauthon <darren@cauthon.com>
2011-08-22 00:06:38 -0500

    More work on admin accounts.

--- src/Bennington.AdminAccounts/Models/AdminAccountListPageViewModelMapper.cs
+++ src/Bennington.AdminAccounts/Models/AdminAccountListPageViewModelMapper.cs
@@ -1,11 +1,22 @@
-﻿using AutoMapperAssist;
-using Bennington.AdminAccounts.Controllers;
+﻿using System.Collections.Generic;
 using Bennington.AdminAccounts.Mappers;
 
 namespace Bennington.AdminAccounts.Models
 {
-    public class AdminAccountListPageViewModelMapper : Mapper<AdminAccount, AdminAccountListPageItem>,
-                                                       IAdminAccountListPageViewModelMapper
+    public class AdminAccountListPageViewModelMapper : IAdminAccountListPageViewModelMapper
     {
+        public IEnumerable<AdminAccountListPageItem> CreateSet(IEnumerable<AdminAccount> adminAccounts)
+        {
+            var list = new List<AdminAccountListPageItem>();
+            foreach (var adminAccount in adminAccounts)
+                list.Add(new AdminAccountListPageItem
+                             {
+                                 FirstName = adminAccount.FirstName,
+                                 Id = adminAccount.Id.ToString(),
+                                 LastName = adminAccount.LastName,
+                                 Username = adminAccount.Username
+                             });
+            return list;
+        }
     }
 }
