b8723bd337089c49b1c1bc0c63682e3f06e1b2a3
Darren Cauthon <darren@cauthon.com>
2011-08-28 14:09:51 -0500

    Add flash static method.

---
+++ src/Bennington.Cms/Flash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Bennington.Cms
+{
+    public static class Flash
+    {
+        private static Action<string> notificationFunction;
+
+        public static void SetNotificationFunction(Action<string> notificationFunction)
+        {
+            Flash.notificationFunction = notificationFunction;
+        }
+
+        public static void Notify(string value)
+        {
+            if (value != null)
+                notificationFunction(value);
+        }
+    }
+}
