3fe9d7eb3c7a2c45c6e1b9fa688c7dbd659e1c0e
Ferris Lucas <lucasf@digitalev.com>
2011-10-02 23:44:14 -0500

    create new sample content app

---
+++ src/MvcApplication1/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using MvcTurbine.ComponentModel;
+using MvcTurbine.Unity;
+using MvcTurbine.Web;
+
+namespace MvcApplication1
+{
+    public class MvcApplication : TurbineApplication
+    {
+        static MvcApplication()
+        {
+            ServiceLocatorManager.SetLocatorProvider(() => new UnityServiceLocator());
+        }
+    }
+}
