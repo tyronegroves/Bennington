3e86a047e9b47d87b34e2dd1275a2a01b3692e16
ferris <lucasf@digitalev.com>
2011-10-06 15:42:49 -0500

    implement rudimentary logout

---
+++ src/Bennington.Cms.PrincipalProvider/HttpModules/LogoutHttpModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Bennington.Cms.PrincipalProvider.HttpModules
+{
+    public class LogoutHttpModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(CheckRequestForLogoutQuerystring);
+        }
+
+        void CheckRequestForLogoutQuerystring(object sender, EventArgs e)
+        {
+            var app = (HttpApplication)sender;
+            if (app.Request.QueryString.AllKeys.Contains("Logout"))
+            {
+                Logout();
+                app.Response.Redirect("~/");
+            }
+        }
+
+        private void Logout()
+        {
+            FormsAuthentication.SignOut();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
