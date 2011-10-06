e41c0f4c9437b7cb5771ec1125e800aae92ad3ff
ferris <lucasf@digitalev.com>
2011-10-06 15:27:45 -0500

    add blade to principal provider assembly to create a dummy admin if there are no users and a dummy admin does not already exist

---
+++ src/Bennington.Cms.PrincipalProvider/AddDummyAdminAccountIfItDoesntExistBlade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Bennington.Cms.PrincipalProvider.Encryption;
+using Bennington.Cms.PrincipalProvider.Models;
+using Bennington.Cms.PrincipalProvider.Repositories;
+using MvcTurbine;
+using MvcTurbine.Blades;
+
+namespace Bennington.Cms.PrincipalProvider
+{
+    public class AddDummyAdminAccountIfItDoesntExistBlade : Blade
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IEncryptionService encryptionService;
+
+        public AddDummyAdminAccountIfItDoesntExistBlade(IUserRepository userRepository,
+                                                        IEncryptionService encryptionService)
+        {
+            this.encryptionService = encryptionService;
+            this.userRepository = userRepository;
+        }
+
+        public override void Spin(IRotorContext context)
+        {
+            var allUsers = userRepository.GetAll().ToArray();
+            if (!allUsers.Where(a => a.Username.Equals("admin", StringComparison.InvariantCultureIgnoreCase)).Any() && allUsers.Count() == 0)
+            {
+                userRepository.SaveAndReturnId(new User()
+                                                   {
+                                                       Id = Guid.NewGuid().ToString(),
+                                                       Username = "admin",
+                                                       Password = encryptionService.Encrypt("admin"),
+                                                   });
+            }
+        }
+    }
+
+    
+}
