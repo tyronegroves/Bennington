3417623de96f24fbf08efa04f55597cc7a3f73e2
Ferris Lucas <ferris.lucas@gmail.com>
2011-10-20 17:32:02 -0500

    correct some SimpleCqrs problems

--- src/Bennington.ContentTree.Domain/Registration/SimpleCqrsRegistration.cs
+++ src/Bennington.ContentTree.Domain/Registration/SimpleCqrsRegistration.cs
@@ -1,5 +1,8 @@
-﻿using MvcTurbine.ComponentModel;
+﻿using System.Configuration;
+using MvcTurbine.ComponentModel;
 using SimpleCqrs.Commanding;
+using SimpleCqrs.EventStore.SqlServer;
+using SimpleCqrs.Eventing;
 
 namespace Bennington.ContentTree.Domain.Registration
 {
@@ -15,6 +18,18 @@ namespace Bennington.ContentTree.Domain.Registration
 	    public void Register(IServiceLocator locator)
 		{
 			benningtonContentTreeSimpleCqrsRuntime.Start();
+
+	        var connectionStringSettings = ConfigurationManager.ConnectionStrings["Bennington.ContentTree.Domain.ConnectionString"];
+            if (connectionStringSettings != null)
+            {
+                benningtonContentTreeSimpleCqrsRuntime.ServiceLocator.Register<IEventStore>(
+                        new SqlServerEventStore(
+                            new SqlServerConfiguration(ConfigurationManager.ConnectionStrings["Bennington.ContentTree.Domain.ConnectionString"].ConnectionString),
+                            new SimpleCqrs.EventStore.SqlServer.Serializers.JsonDomainEventSerializer()));                
+            } else
+            {
+                benningtonContentTreeSimpleCqrsRuntime.ServiceLocator.Register<IEventStore>(new MemoryEventStore());
+            }
 
             var commandBus = benningtonContentTreeSimpleCqrsRuntime.ServiceLocator.Resolve<ICommandBus>();
 			locator.Register(commandBus);
