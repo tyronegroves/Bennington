88a4a9e08578a6cd7e1a02abcdf9f3b8a2c05ec9
Ferris Lucas <lucasf@digitalev.com>
2011-10-21 01:03:46 -0500

    begin caching TreeNodeSummaryContext, IToolLinkContext is next

---
+++ src/Bennington.ContentTree.Cache/ReigsterTreeNodeSummaryContextCacheBlade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bennington.ContentTree.Contexts;
+using MvcTurbine;
+using MvcTurbine.Blades;
+
+namespace Bennington.ContentTree.Cache
+{
+    public class ReigsterTreeNodeSummaryContextCacheBlade : Blade
+    {
+        public override void Spin(IRotorContext context)
+        {
+            var treeNodeSummaryContextCache = context.ServiceLocator.Resolve<TreeNodeSummaryContextCache>();
+            context.ServiceLocator.Register<ITreeNodeSummaryContext>(treeNodeSummaryContextCache);
+        }
+    }
+}
---
+++ src/Bennington.ContentTree.Cache/TreeNodeSummaryContextCache.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bennington.ContentTree.Contexts;
+using Bennington.ContentTree.Models;
+
+namespace Bennington.ContentTree.Cache
+{
+    public class TreeNodeSummaryContextCache : ITreeNodeSummaryContext
+    {
+        private readonly TreeNodeSummaryContext treeNodeSummaryContextImplementation;
+        private readonly ConcurrentDictionary<string, TreeNodeSummary[]> treeNodeSummaryCacheByParentId = new ConcurrentDictionary<string, TreeNodeSummary[]>(); 
+        private readonly ConcurrentDictionary<string, TreeNodeSummary> treeNodeSummaryCacheById = new ConcurrentDictionary<string, TreeNodeSummary>(); 
+
+        public TreeNodeSummaryContextCache(TreeNodeSummaryContext treeNodeSummaryContextImplementation)
+        {
+            this.treeNodeSummaryContextImplementation = treeNodeSummaryContextImplementation;
+        }
+
+        public TreeNodeSummary GetTreeNodeSummaryByTreeNodeId(string nodeId)
+        {
+            TreeNodeSummary treeNodeSummary;
+            if (!treeNodeSummaryCacheById.TryGetValue(nodeId, out treeNodeSummary))
+            {
+                treeNodeSummary = treeNodeSummaryContextImplementation.GetTreeNodeSummaryByTreeNodeId(nodeId);
+                treeNodeSummaryCacheById.TryAdd(nodeId, treeNodeSummary);
+            }
+            
+            return treeNodeSummary;
+        }
+
+        public IEnumerable<TreeNodeSummary> GetChildren(string parentNodeId)
+        {
+            TreeNodeSummary[] treeNodeSummaries;
+            if (!treeNodeSummaryCacheByParentId.TryGetValue(parentNodeId, out treeNodeSummaries))
+            {
+                treeNodeSummaries = treeNodeSummaryContextImplementation.GetChildren(parentNodeId).ToArray();
+                treeNodeSummaryCacheByParentId.TryAdd(parentNodeId, treeNodeSummaries);
+            }
+            return treeNodeSummaries;
+        }
+
+        public string Create(string parentNodeId, string providerTypeAssemblyQualifiedName)
+        {
+            return treeNodeSummaryContextImplementation.Create(parentNodeId, providerTypeAssemblyQualifiedName);
+        }
+    }
+}
