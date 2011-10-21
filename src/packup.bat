80e72b1c6d42bb3e2c26156410d840f34c4167b1
Ferris Lucas <lucasf@digitalev.com>
2011-10-21 13:25:53 -0500

    add ability to override cms masterpage

--- src/packup.bat
+++ src/packup.bat
@@ -19,3 +19,4 @@ copy "C:\_Application\Bennington\src\Bennington.Repository\bin\Debug\Bennington.
 copy "C:\_Application\Bennington\src\Bennington.Repository\bin\Debug\Bennington.Repository.pdb" "C:\_Application\Bennington\localWorkingFolder\Deploy"
 copy "C:\_Application\Bennington\src\Bennington.ContentTree.TreeManager\bin\Debug\Bennington.ContentTree.TreeManager.dll" "C:\_Application\Bennington\localWorkingFolder\Deploy"
 copy "C:\_Application\Bennington\src\Bennington.ContentTree.TreeManager\bin\Debug\Bennington.ContentTree.TreeManager.pdb" "C:\_Application\Bennington\localWorkingFolder\Deploy"
+copy "C:\_Application\Bennington\src\Bennington.ContentTree.Providers.ToolLinkNodeProvider\bin\Bennington.ContentTree.Providers.ToolLinkNodeProvider.dll" "C:\_Application\Bennington\localWorkingFolder\Deploy"
